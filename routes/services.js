28fede8a194c195ba9c3fcdaa53a14b1fdf2d6d6
gildachoquequena <gildachoquequena@gmail.com>
2018-11-28 22:24:05 +0100

    respondiendo pregunta 1 el inciso c

--- routes/services.js
+++ routes/services.js
@@ -8,7 +8,7 @@ router.get('/', function(req, res) {
 //return;
 var host = req.headers.host;
 var baseUrl = "no pude encontrar baseUrl";
-var originalUrl = req.originalUrl;a url bas
+var originalUrl = req.originalUrl;
 var httpVersion = req.httpVersion;
 var _startTime = req._startTime;
 var respuesta = "nose encuentra baseUrl, originalUrl devuelve la url original de la solicitud"
@@ -23,4 +23,32 @@ res.status(200).json({
 });
 });
 
+router.post('/interes', function(req,res,next) {
+ var monto_inicial= req.body.monto_inicial;
+ var porcentaje_anual= req.body.porcentaje_anual;
+ if (monto_inicial == 0 || porcentaje_anual == 0 || monto_inicial == undefined || porcentaje_anual==undefined){
+   res.status(400).json({msn : "Error al introducir las variables"});
+ }
+ var X = parseFloat(monto_inicial);
+ var Y = parseFloat(porcentaje_anual);
+ var final =0;
+ final=((Y/100)*X)+(X);
+ var Año=0;
+ Año=(Y*X)/100;
+ var mes=Año/12;
+ var semana=Año/52;
+ var dia=Año/365;
+      res.status(200).json({
+           "Monto Final":final,
+           "Monto Anual":Año,
+           "Monto Mensual":mes,
+           "MOnto Semanal":semana,
+           "Monto Diario":dia
+ });
+
+
+
+
+
+});
 module.exports = router;
